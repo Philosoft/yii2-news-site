9f58b126100bf40cc12fd1c685126a48752b7226
Alexander Frolov <thephilosoft@gmail.com>
2017-08-29 18:01:41 +0300

    add local db config
    from now on, there is an ability to add `@app/config/db-local.php` file, which will overwrite default `@app/config/db.php`

--- config/db.php
+++ config/db.php
@@ -1,9 +1,21 @@
 <?php
 
-return [
+$defaultDB = [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
 ];
+
+$localDBConfig = __DIR__ . "/db-local.php";
+$localDB = [];
+
+if (is_readable($localDBConfig)) {
+    $localDB = require($localDBConfig);
+}
+
+return array_merge(
+    $defaultDB,
+    $localDB
+);
